58f2f829ddb4b89c8846f9e79fc8788ce3dd53f5
AnshumaanGupta <anshuman.vinay@gmail.com>
2024-07-31 03:11:01 +0530

    Added INR currency

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -11,7 +11,7 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Text('\$${spendingAmount.toStringAsFixed(0)}'),
+        Text('₹${spendingAmount.toStringAsFixed(0)}'),
         SizedBox(
           height: 4,
         ),
